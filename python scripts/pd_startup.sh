24a834e805857c2038c7dc1afb9aa54d87741b37
Your Name <you@example.com>
2018-08-23 14:33:07 +0100

    final

--- python scripts/pd_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# this script will start Pd in no gui mode
-
-sudo -H -u pi bash -c bash "echo 'starting Pd now'"
-pd -nogui /home/pi/loppi/step-vibrato.pd &
-
-/home/pi/loppi/lop2pd.py &
